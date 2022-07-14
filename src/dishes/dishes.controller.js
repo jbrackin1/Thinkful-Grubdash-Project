d1e2143395dab08c9d2bb99595917d38d31ed928
Jerod Brackin <jbrackin1@gmail.com>
2022-07-14 15:13:58 -0500

    updated function naming conventions

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -3,7 +3,7 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 //Functional Middleware functions:
-const dishExists = (req, res, next) => {
+function dishExists(req, res, next){
    const dishId = req.params.dishId;
    res.locals.dishId = dishId;
    const foundDish = dishes.find((dish) => dish.id === dishId);
@@ -15,7 +15,7 @@ const dishExists = (req, res, next) => {
    res.locals.dish = foundDish;
 };
 
-const dishValidName = (req, res, next) => {
+function dishValidName(req, res, next){
    const { data = null } = req.body;
    res.locals.newDD = data;
    const dishName = data.name;
@@ -27,7 +27,7 @@ const dishValidName = (req, res, next) => {
    }
 };
 
-const dishHasValidDescription = (req, res, next) => {
+function dishHasValidDescription(req, res, next){
    const dishDescription = res.locals.newDD.description;
    if (!dishDescription || dishDescription.length === 0) {
       return next({
@@ -37,7 +37,7 @@ const dishHasValidDescription = (req, res, next) => {
    }
 };
 
-const dishHasValidPrice = (req, res, next) => {
+function dishHasValidPrice(req, res, next){
    const dishPrice = res.locals.newDD.price;
    if (!dishPrice || typeof dishPrice != "number" || dishPrice <= 0) {
       return next({
@@ -47,7 +47,7 @@ const dishHasValidPrice = (req, res, next) => {
    }
 };
 
-const dishHasValidImage = (req, res, next) => {
+function dishHasValidImage(req, res, next) {
    const dishImage = res.locals.newDD.image_url;
    if (!dishImage || dishImage.length === 0) {
       return next({
@@ -57,7 +57,7 @@ const dishHasValidImage = (req, res, next) => {
    }
 };
 
-const dishIdMatches = (req, res, next) => {
+function dishIdMatches(req, res, next){
    const paramId = res.locals.dishId;
    const { id = null } = res.locals.newDD;
    if (paramId != id && id) {
@@ -69,7 +69,7 @@ const dishIdMatches = (req, res, next) => {
 };
 
 //Clarity Middleware Functions
-const createValidation = (req, res, next) => {
+function createValidation(req, res, next) {
    dishValidName(req, res, next);
    dishHasValidDescription(req, res, next);
    dishHasValidPrice(req, res, next);
@@ -77,12 +77,12 @@ const createValidation = (req, res, next) => {
    next();
 };
 
-const readValidation = (req, res, next) => {
+function readValidation(req, res, next){
    dishExists(req, res, next);
    next();
 };
 
-const updateValidation = (req, res, next) => {
+function updateValidation(req, res, next){
    dishExists(req, res, next);
    dishValidName(req, res, next);
    dishHasValidDescription(req, res, next);
